3f0256f5d0052ab24343ad70e93f7db7b255ff8b
wataru4635 <wataemonn1014@gmail.com>
2023-11-16 13:57:19 +0900

    FV前修正

--- WordPressTheme/archive-campaign.php
+++ WordPressTheme/archive-campaign.php
@@ -17,70 +17,72 @@
     </div>
   </section>
 
-  <?php get_template_part('parts/breadcrumb') ?>
+  <?php get_template_part('parts/breadcrumb'); ?>
 
   <section class="page-campaign sub-page-campaign">
     <div class="page-campaign__inner inner">
       <div class="page-campaign__tab common-tab">
         <ul class="common-tab__lists">
-          <li class="common-tab__list is-active"><a href="<?php echo get_post_type_archive_link('campaign') ?>">ALL</a></li>
+          <li class="common-tab__list is-active"><a href="<?php echo get_post_type_archive_link('campaign'); ?>">ALL</a></li>
           <?php $course_terms = get_terms('campaign_category', array('hide_empty'=>false)); ?>
-          <?php foreach($course_terms as $course_term ) : ?>
+          <?php foreach($course_terms as $course_term) : ?>
             <li class="common-tab__list"><a href="<?php echo get_term_link($course_term,'campaign_category'); ?>"><?php echo $course_term->name; ?></a></li>
           <?php endforeach; ?>
         </ul>
       </div>
 
       <div class="page-campaign__container">
-        <div class="page-campaign__cards">
-          <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-            <div class="page-campaign__card campaign-card">
-              <div class="campaign-card__img">
-                <?php if (has_post_thumbnail()) : ?>
-                  <?php the_post_thumbnail(); ?>
-                <?php else : ?>
-                  <img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/noimg.png" alt="<?php the_title(); ?>アイキャッチ画像">
-                <?php endif; ?>
-              </div>
-
-              <div class="campaign-card__inner">
-                <div class="campaign-card__head">
-                  <p class="campaign-card__label"><?php echo get_the_terms(get_the_ID(), 'campaign_category')[0]->name; ?></p>
-                  <h3 class="campaign-card__title"><?php the_title(); // タイトルを表示 ?></h3>
+        <?php if (have_posts()) : ?>
+          <div class="page-campaign__cards">
+            <?php while (have_posts()) : the_post(); ?>   
+              <div class="page-campaign__card campaign-card">
+                <div class="campaign-card__img">
+                  <?php if (has_post_thumbnail()) : ?>
+                    <?php the_post_thumbnail(); ?>
+                  <?php else : ?>
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/common/noimg.png" alt="<?php the_title(); ?>アイキャッチ画像">
+                  <?php endif; ?>
                 </div>
 
-                <?php $campaignPrice = get_field('campaign-price');
-                if ($campaignPrice) :?>
-                  <div class="campaign-card__body">
-                    <p class="campaign-card__text"><?php echo $campaignPrice['campaign-price_1']; ?></p>
-                    <div class="campaign-card__price">
-                      <p class="campaign-card__price-before">¥<?php echo $campaignPrice['campaign-price_2']; ?></p>
-                      <p class="campaign-card__price-after">¥<?php echo $campaignPrice['campaign-price_3']; ?></p>
-                    </div>
+                <div class="campaign-card__inner">
+                  <div class="campaign-card__head">
+                    <p class="campaign-card__label"><?php echo get_the_terms(get_the_ID(), 'campaign_category')[0]->name; ?></p>
+                    <h3 class="campaign-card__title"><?php the_title(); // タイトルを表示 ?></h3>
                   </div>
-                <?php endif; ?>
 
-                <?php $campaignDescription = get_field('campaign-description');
-                if ($campaignDescription) :?>
-                  <p class="campaign-card__detail u-desktop">
-                    <?php echo $campaignDescription['campaign-description_1']; ?>
-                  </p>
-                  <div class="campaign-card__footer u-desktop">
-                    <?php $campaignDescriptionTime = $campaignDescription['campaign-description-time'];
-                    if ($campaignDescriptionTime) : ?>
-                      <p class="campaign-card__date"><?php echo $campaignDescriptionTime['campaign-description-time_1']; ?>&thinsp;-&thinsp;<?php echo $campaignDescriptionTime['campaign-description-time_2']; ?></p>
-                    <?php endif; ?>
-
-                    <p class="campaign-card__contact">ご予約・お問い合わせはコチラ</p>
-                    <div class="campaign-card__button">
-                      <a href="<?php echo esc_url(home_url('/contact/')); ?>" class="section-button">Contact us<span></span></a>
+                  <?php $campaignPrice = get_field('campaign-price');
+                  if ($campaignPrice) : ?>
+                    <div class="campaign-card__body">
+                      <p class="campaign-card__text"><?php echo $campaignPrice['campaign-price_1']; ?></p>
+                      <div class="campaign-card__price">
+                        <p class="campaign-card__price-before">¥<?php echo $campaignPrice['campaign-price_2']; ?></p>
+                        <p class="campaign-card__price-after">¥<?php echo $campaignPrice['campaign-price_3']; ?></p>
+                      </div>
                     </div>
-                  </div>
-                <?php endif; ?>
+                  <?php endif; ?>
+
+                  <?php $campaignDescription = get_field('campaign-description');
+                  if ($campaignDescription) : ?>
+                    <p class="campaign-card__detail u-desktop">
+                      <?php echo $campaignDescription['campaign-description_1']; ?>
+                    </p>
+                    <div class="campaign-card__footer u-desktop">
+                      <?php $campaignDescriptionTime = $campaignDescription['campaign-description-time'];
+                      if ($campaignDescriptionTime) : ?>
+                        <p class="campaign-card__date"><?php echo $campaignDescriptionTime['campaign-description-time_1']; ?>&thinsp;-&thinsp;<?php echo $campaignDescriptionTime['campaign-description-time_2']; ?></p>
+                      <?php endif; ?>
+
+                      <p class="campaign-card__contact">ご予約・お問い合わせはコチラ</p>
+                      <div class="campaign-card__button">
+                        <a href="<?php echo esc_url(home_url('/contact/')); ?>" class="section-button">Contact us<span></span></a>
+                      </div>
+                    </div>
+                  <?php endif; ?>
+                </div>
               </div>
-            </div>
-          <?php endwhile; endif;?>
-        </div>
+            <?php endwhile; ?>
+          </div>
+        <?php endif; ?>
       </div>
 
       <div class="page-campaign__pagination">
@@ -91,5 +93,4 @@
 </main>
 
 <?php get_footer(); ?>
-
 
